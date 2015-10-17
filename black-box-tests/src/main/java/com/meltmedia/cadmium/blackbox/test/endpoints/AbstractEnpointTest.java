69e0bbdae744033a1ec14173c72e2d94f3c10156
DigitalGalaxy <DigitalGalaxy@users.noreply.github.com>
2015-10-16 18:40:37 -0700

    Update AbstractEnpointTest.java

--- black-box-tests/src/main/java/com/meltmedia/cadmium/blackbox/test/endpoints/AbstractEnpointTest.java
+++ black-box-tests/src/main/java/com/meltmedia/cadmium/blackbox/test/endpoints/AbstractEnpointTest.java
@@ -27,6 +27,63 @@ import java.util.Map;
  *
  * @author John McEntire
  */
+ 
+ 
+ 
+ /** 
+ * 
+ * ^^^^ I'm sure this seemed like a reasonable comment to the author. 
+ * And the variables are all descriptively named, which is a huge plus.
+ * The problem is, that even though this inline comment informs me what 
+ * function this code is supposed to perform, it does not tell me how it 
+ * performs it.  I have three classes that are being called, and a Map, 
+ * (whatever that is, a custom class?  Something I have to dig around SackOverflow
+ * for?) and then it goes into a ton of random method calls that I have no 
+ * idea what they are for.  With nothing to go off of.  
+ * 
+ * Here's the thing.  Inline comments are not for people who have tons of time who are smarter
+ * /more skilled than you. Inline comments are for people who are less skilled that you who 
+ * want to try to make sense of what is in front of them in the shortest reasonable time
+ * possible.  So pretend, just for moment, that the next monkey who reads your code is a little
+ * less skilled than yourself, and would appreciate hand-holding inline comments.  
+ * Otherwise, you end up like this: https://s3.amazonaws.com/uploads.hipchat.com/23199/2470072/fQlmuSda6SGxVmS/upload.png
+ * Or this: https://s3.amazonaws.com/uploads.hipchat.com/23199/2470072/BX3KV1IVEPj1vF5/upload.png
+ * In case you can't click those pictures, they are of a small monkey and a cat trying to untangle a ball of yarn, respectively.  
+ * I'm sure this is great logic that makes a lot of sense.  It's is also clear as mud.  I coudl just bug a dev... 
+ * But I know the senior devs are busy and I don't want to pester them
+ * every ten minutes every time I don't understand some method call.  
+ * 
+ * A novel is not necessary, people, just something, a quick sentence.  One extra line every few methods.
+ * To tell you what a given method is for, and maybe if it links to something else, or if changing it is likely to break something else.
+ * Maybe a rationale for why it is there if that is not immidiately obvious?
+ * 
+ * Code is sharing low-level intelligence, not only between metallic computers but between biological comptuers, us monkey brains
+ * too. And until us monkeys become metallic computers ourselves, it should be written as such. Half for the metallic brain we are interfacing
+ * with more or less directly, and half for the biological brain of a fellow coder who we may be interfacing with indirectly when somebody else reads our code.
+ * We, as coders, should spread the intelligence, and we should not obfuscate our code to those less skilled by leaving it in a lump.
+ * Lack of inline comments raises the barrier to entry, and while that barrier may be easy for more senior coders familiar with the project to jump over, it's not
+ * as easy to newcomers or those coming from another language.  Which is great if it's a program for yourself only.  But, if the code is on
+ * GitHub and anybody can see it, or even if it's office code and eventually somebody else will come in and take over the project, then some other monkey brain 
+ * is going to have to figure this out, and backtrack every single line.  (Besides, even the author of any given piece of code is going to come in with a hangover one day,
+ * and due to the aforementioned hangover, is going to have no idea what he even wrote.  Even he will benefit from inline comments!)
+ * 
+ * This is a call to all programmers everywhere.  Civilization as we know it is an impossiblity without the things we create every day. 
+ * Without computers we go back a century and we stay there.  Let's make it easier on ourselves, save every one of us a lot of 
+ * trouble and wasted neural activity, and COMMENT OUR CODE!!!!
+ * SAY it with me!
+ * COMMENT 
+ * OUR 
+ * CODE
+ *
+ * Ok, probably could have deciphered 4 or 5 method calls in the time it took to rant about inline comments!  
+ * 
+ * All right, there are no inline comments here.  So, time to figure this out! To IntelliJ!
+ * 
+ * What makes the magic Cadmium button press, anyways? 
+ * 
+ * */
+
+ 
 public abstract class AbstractEnpointTest implements EndpointTest {
 
   protected ApiRequest request;
